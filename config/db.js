92213bc07814b64b0bfdfc66718f72735f67080d
Santoshshriwas <santoshdhobi9589@gmail.com>
2024-07-25 15:26:21 +0530

    Deploy problem

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config()
 export const  connectDB = async () =>{
     await mongoose.connect('mongodb://127.0.0.1:27017/food-del').then(()=>console.log("DB Connected"))
 }
