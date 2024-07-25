f57fc1fe1cdc08ff62e289c51af779595d7b6e08
Santoshshriwas <santoshdhobi9589@gmail.com>
2024-07-25 19:58:31 +0530

    Solve

--- config/db.js
+++ config/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 
+// export const  connectDB = async () =>{
+//     await mongoose.connect('mongodb://127.0.0.1:27017/food-del').then(()=>console.log("DB Connected"))
+// }
+
+
 export const  connectDB = async () =>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/food-del').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://santoshdhobi958905:YbJlFTGNdhvCNPSP@cluster0.yy2cgr4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"))
 }
 
 
